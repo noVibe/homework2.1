2ac45bc92266106992a2be54ea18fb6e3ccc4bd7
Василий <remindscope@gmail.com>
2023-01-11 20:36:57 +0300

    Utility class added

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,5 @@
+import static Utilities.Utilities.*;
+
 public class Car {
     private String brand;
     private String model;
@@ -7,13 +9,14 @@ public class Car {
     private String country;
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        this.brand = brand == null || brand.isEmpty() ? "default" : brand;
-        this.model = model == null || model.isEmpty() ? "default" : model;
-        this.country = country == null || country.isEmpty() ? "default" : country;
+        this.brand = isNullOrEmpty(brand) ? "default" : brand;
+        this.model = isNullOrEmpty(model) || model.isEmpty() ? "default" : model;
+        this.country = isNullOrEmpty(country) || country.isBlank() ? "default" : country;
         this.engineVolume = engineVolume > 0 ? engineVolume : 1.5;
-        this.color = color == null || color.isEmpty() ? "white" : color;
+        this.color = isNullOrEmpty(color) || color.isEmpty() ? "white" : color;
         this.year = year > 0 ? year : 2000;
-       }
+    }
+
 
     @Override
     public String toString() {
---
+++ src/Utilities/Utilities.java
@@ -0,0 +1,8 @@
+package Utilities;
+
+public class Utilities {
+    public static int a = 0;
+    public static boolean isNullOrEmpty(String input) {
+        return (input == null || input.isEmpty());
+    }
+}
