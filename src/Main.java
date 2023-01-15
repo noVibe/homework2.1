a98496bdc1273354bf3b64390a402f0a0673bd63
Василий <remindscope@gmail.com>
2023-01-15 18:03:22 +0300

    Utilities class refactored. Added regex for plateNumber.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import transport.Car;
 public class Main {
     public static void main(String[] args) {
 
-        Car car = new Car("Audi", "A8 50 L TDI quattro", 3,"black",2020,
-                "Germany", "Sedan", 4, "Robot", "B055", false, true, false);
+        Car car = new Car("Audi", "A8 50 L TDI quattro", 2000,"black",2020,
+                "Germany", "Sedan", 4, "Robot", "а222ок202", false, true, false);
         Car test = new Car("", null, 0, null,-1, "", null,
                 100, "", null, false, false, false);
 
